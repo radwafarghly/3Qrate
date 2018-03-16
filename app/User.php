aa6884346401b54c028e56db35da57b23d4d2816
radwafarghly <radwa.a.farghly@gmail.com>
2018-03-16 14:45:06 +0200

    confirm email

---
+++ app/User.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+    use EntrustUserTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','admin','verified','email_token',
+    ];
+
+    public function book()
+    {
+        return $this->hasMany('App\Book');
+    }
+    
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+     public function verified()
+    {
+        $this->verified = 1;
+        $this->email_token = null;
+        $this->save();
+    }
+}
---
+++ resources/views/layouts/ourteam.blade.php
@@ -0,0 +1,78 @@
+<h1 class="text-center">Meet Our Team</h1>
+<div style="margin-top:10px; background-color:#eee"  class="container-fluid section-ourTeam">
+<!--<div class="row">
+    <div style="background-image:url({{ asset('/images/hands-joined-by-the-team_1150-100.jpg')}})" class="col-md-12 col-sm-12 col-xs-12 ourTeam-hedding text-center">
+        <h1>Meet Our Team</h1>
+    </div>
+</div>-->
+<div class="row">
+<div class="col-md-3 col-sm-3 col-xs-12">
+        <div class="row section-success ourTeam-box text-center">
+            <div class="col-md-12 section1">
+                <img src="{{ asset('/images/111.png')}}">
+            </div>
+            <div class="col-md-12 section2">
+                <p>Ayat Samir</p><br>
+                <h1>Full-Stack Developer</h1><br>
+            </div>
+            <div class="col-md-12 section3">
+                <p>
+                We are one team , we cooperate with each other to create this website in the best status
+                </p>
+            </div>
+          
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-3 col-xs-12">
+        <div class="row section-danger ourTeam-box text-center">
+            <div class="col-md-12 section1">
+                <img src="{{ asset('/images/mo.jpg')}}">
+            </div>
+            <div class="col-md-12 section2">
+                <p>Mohamed Salah</p><br>
+                <h1>Full-Stack Developer</h1><br>
+            </div>
+            <div class="col-md-12 section3">
+                <p>
+                We are one team , we cooperate with each other to create this website in the best status
+                </p>
+            </div>
+          
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-3 col-xs-12">
+        <div class="row section-success ourTeam-box text-center">
+            <div class="col-md-12 section1">
+            <img src="{{ asset('/images/IMG-20161018-WA0010.jpg')}}">
+            </div>
+            <div class="col-md-12 section2">
+                <p>Radwa Ahmed</p><br>
+                <h1>Full-Stack Developer</h1><br>
+            </div>
+            <div class="col-md-12 section3">
+                <p>
+                We are one team , we cooperate with each other to create this website in the best status
+                </p>
+            </div>
+         
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-3 col-xs-12">
+        <div class="row section-danger ourTeam-box text-center">
+            <div class="col-md-12 section1">
+            <img src="{{ asset('/images/amr.jpg')}}">
+            </div>
+            <div class="col-md-12 section2">
+                <p>AMR SABER</p><br>
+                <h1>Full-Stack Developer</h1>
+            </div>
+            <div class="col-md-12 section3">
+                <p>
+                We are one team , we cooperate with each other to create this website in the best status .
+                </p>
+            </div>
+        
+        </div>
+    </div>
+</div>
+</div>
